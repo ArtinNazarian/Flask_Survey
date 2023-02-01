58f40b542bb55588f9bbb186854bb8d33e1f254f
Artin <artin.n31@gmail.com>
2023-02-01 09:10:35 -0800

    second commit

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
-
 
 
 app = Flask(__name__)
@@ -10,35 +9,31 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
-response =[]
+
 
 @app.route('/')
 def home():
     title = satisfaction_survey.title
-    instructions = satisfaction_survey.instructions
-      
+    instructions = satisfaction_survey.instructions          
     return render_template('home.html', title=title, instructions=instructions)
 
 
 @app.route('/start', methods=["POST"])
-def start():
-    
+def start():    
+    session['responses']=[]
     return redirect('/questions/0')
 
 
 @app.route('/questions/<int:id>')
-def questions(id):       
+def questions(id):    
+    responses = session.get('responses')   
   
-    if len(response) == len(satisfaction_survey.questions):
+    if len(responses) == len(satisfaction_survey.questions):
         return redirect('/complete')
         
-    # if id != len(response):
-    #     return redirect(f"/questions/{len(response)}")
-
-    if len(response) != id:
-        # Trying to access questions out of order.
+    if len(responses) != id:      
         flash(f"Invalid question id: {id}.")
-        return redirect(f"/questions/{len(response)}")
+        return redirect(f"/questions/{len(responses)}")
 
     survey_question = satisfaction_survey.questions[id]
     return render_template('questions.html', survey_question=survey_question)
@@ -47,13 +42,18 @@ def questions(id):
 @app.route('/answer', methods=['POST'] )
 def answer():
     answer = request.form['answer']
-    response.append(answer)
-    if len(response) == len(satisfaction_survey.questions):
+    responses = session['responses']
+    responses.append(answer)
+    session['responses'] = responses
+    
+    if len(responses) == len(satisfaction_survey.questions):
         return redirect('/complete')
     else:
-        return redirect(f"/questions/{len(response)}")
+        return redirect(f"/questions/{len(responses)}")
 
 @app.route('/complete')
 def complete():
     return render_template('thank_you.html')
+
+
     
